dcaaad7383db479a2d2b41bdbbcdeb43a6cbe1bd
Joshuaashley-git <Ashjoshuaashley@gmail.com>
2026-06-30 13:33:50 +0530

    Delete Design Patterns and principles/FactoryMethodPatternExample(Ex2) copy directory

--- Design Patterns and principles/FactoryMethodPatternExample(Ex2) copy/src/DocumentFactory.java
+++
@@ -1,9 +0,0 @@
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-
-    public Document openNewDocument(){
-        Document doc=createDocument();
-        doc.open();
-        return doc;
-    }
-}
