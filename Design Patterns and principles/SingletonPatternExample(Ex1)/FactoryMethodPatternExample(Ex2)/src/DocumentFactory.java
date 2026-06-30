dba6123e80fa672f7ee3a3001ec72aa2fb8b244e
Joshuaashley-git <Ashjoshuaashley@gmail.com>
2026-06-30 13:28:40 +0530

    Add files via upload

---
+++ Design Patterns and principles/SingletonPatternExample(Ex1)/FactoryMethodPatternExample(Ex2)/src/DocumentFactory.java
@@ -0,0 +1,9 @@
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+
+    public Document openNewDocument(){
+        Document doc=createDocument();
+        doc.open();
+        return doc;
+    }
+}
