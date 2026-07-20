e330394d3a9214bff0804f9a58ebcf6dc0dfcf77
Joshuaashley-git <Ashjoshuaashley@gmail.com>
2026-07-20 10:09:53 +0530

    Add files via upload

---
+++ React/3.ReactJS-HOL/src/App.js
@@ -0,0 +1,11 @@
+import { CalculatorScore } from "./Components/CalculatorScore";
+
+function App() {
+  return (
+    <div>
+      <CalculatorScore Name="Joshua Ashley Raymond" School="Panimalar Engineering College" total={300} sub={3} />
+    </div>
+  );
+}
+
+export default App;
